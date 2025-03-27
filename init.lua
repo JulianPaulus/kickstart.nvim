e06c784d26df041c0e77d8a5da325a05a6915fa7
Julian Paulus <julianpaulus704@gmail.com>
2025-03-27 13:51:25 +0100

    add bufferline

--- init.lua
+++ init.lua
@@ -147,6 +147,34 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
 
   -- NOTE: Custom Plugins
+
+  {
+    'akinsho/bufferline.nvim',
+    version = '*',
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    config = function()
+      local bufferline = require 'bufferline'
+      bufferline.setup()
+
+      vim.keymap.set({ 'n', 'v' }, '<S-h>', '<Cmd>:BufferLineCyclePrev<CR>', { desc = 'Select previous bufferline' })
+      vim.keymap.set({ 'n', 'v' }, '<S-l>', '<Cmd>:BufferLineCycleNext<CR>', { desc = 'Select next bufferline' })
+      vim.keymap.set({ 'n', 'v' }, '<S-x>', '<Cmd>:bd<CR>', { desc = 'Close Bufferline' })
+
+      vim.keymap.set({ 'n', 'v' }, '<C-h>', function()
+        bufferline.move(-1)
+      end, { desc = 'Move Tab left' })
+      vim.keymap.set({ 'n', 'v' }, '<C-l>', function()
+        bufferline.move(1)
+      end, { desc = 'Move Tab right' })
+
+      for i = 1, 9 do
+        vim.keymap.set('n', '<leader>' .. i, function()
+          bufferline.go_to(i, false)
+        end)
+      end
+    end,
+  },
+
   { 'ellisonleao/gruvbox.nvim', priority = 1000, config = true },
   { 'unblevable/quick-scope' },
 
@@ -170,11 +198,11 @@ require('lazy').setup({
         harpoon.ui:toggle_quick_menu(harpoon:list())
       end)
 
-      for i = 1, 9 do
-        vim.keymap.set('n', '<leader>' .. i, function()
-          harpoon:list():select(i)
-        end)
-      end
+      -- for i = 1, 9 do
+      --   vim.keymap.set('n', '<leader>' .. i, function()
+      --     harpoon:list():select(i)
+      --   end)
+      -- end
     end,
   },
 
